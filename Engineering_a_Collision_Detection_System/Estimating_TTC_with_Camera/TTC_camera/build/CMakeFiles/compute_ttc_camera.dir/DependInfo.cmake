1d62dc8a44316263b8ff866afbf3fb8d745db521
albert <albertfrancis0703@gmail.com>
2026-01-07 09:30:51 +0100

    chapter 3 complete

---
+++ Engineering_a_Collision_Detection_System/Estimating_TTC_with_Camera/TTC_camera/build/CMakeFiles/compute_ttc_camera.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/albert/SENSOR_FUSION/SFND_Camera_Project/Engineering_a_Collision_Detection_System/Estimating_TTC_with_Camera/TTC_camera/src/compute_ttc_camera.cpp" "CMakeFiles/compute_ttc_camera.dir/src/compute_ttc_camera.cpp.o" "gcc" "CMakeFiles/compute_ttc_camera.dir/src/compute_ttc_camera.cpp.o.d"
+  "/home/albert/SENSOR_FUSION/SFND_Camera_Project/Engineering_a_Collision_Detection_System/Estimating_TTC_with_Camera/TTC_camera/src/structIO.cpp" "CMakeFiles/compute_ttc_camera.dir/src/structIO.cpp.o" "gcc" "CMakeFiles/compute_ttc_camera.dir/src/structIO.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
